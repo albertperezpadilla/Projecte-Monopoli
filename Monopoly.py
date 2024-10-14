a38ea5c6a8f80f61040b083ae1f9b861c998643d
Albert Pérez Padilla <152429134+albertperezpadilla@users.noreply.github.com>
2024-10-14 16:32:34 +0200

    Direccionario de diccionarios de precios

--- Monopoly.py
+++ Monopoly.py
@@ -11,3 +11,119 @@ dgroc = 2000
 dtaronja = 2000
 dvermell = 2000
 dblau= 2000
+
+#Direccionario de direccionarios de los precios de cada calle
+llista_preus = {
+    "Lauria": {
+        "Ll. Casa": 10,
+        "Ll. Hotel": 15,
+        "Cmp. Trrny": 50,
+        "Cmp. Casa": 200,
+        "Cmp. Hotel": 250
+    },
+    "Roselló": {
+        "Ll. Casa": 10,
+        "Ll. Hotel": 15,
+        "Cmp. Trrny": 50,
+        "Cmp. Casa": 225,
+        "Cmp. Hotel": 255
+    },
+    "Marina": {
+        "Ll. Casa": 15,
+        "Ll. Hotel": 15,
+        "Cmp. Trrny": 50,
+        "Cmp. Casa": 250,
+        "Cmp. Hotel": 260
+    },
+    "C. de cent": {
+        "Ll. Casa": 15,
+        "Ll. Hotel": 20,
+        "Cmp. Trrny": 50,
+        "Cmp. Casa": 275,
+        "Cmp. Hotel": 265
+    },
+    "Muntaner": {
+        "Ll. Casa": 20,
+        "Ll. Hotel": 20,
+        "Cmp. Trrny": 60,
+        "Cmp. Casa": 300,
+        "Cmp. Hotel": 270
+    },
+    "Aribau": {
+        "Ll. Casa": 20,
+        "Ll. Hotel": 20,
+        "Cmp. Trrny": 60,
+        "Cmp. Casa": 325,
+        "Cmp. Hotel": 275
+    },
+    "Sant Joan": {
+        "Ll. Casa": 25,
+        "Ll. Hotel": 25,
+        "Cmp. Trrny": 60,
+        "Cmp. Casa": 350,
+        "Cmp. Hotel": 280
+    },
+    "Aragó": {
+        "Ll. Casa": 25,
+        "Ll. Hotel": 25,
+        "Cmp. Trrny": 50,
+        "Cmp. Casa": 375,
+        "Cmp. Hotel": 285
+    },
+    "Urquinaona": {
+        "Ll. Casa": 30,
+        "Ll. Hotel": 25,
+        "Cmp. Trrny": 70,
+        "Cmp. Casa": 400,
+        "Cmp. Hotel": 290
+    },
+    "Fontana": {
+        "Ll. Casa": 30,
+        "Ll. Hotel": 30,
+        "Cmp. Trrny": 70,
+        "Cmp. Casa": 425,
+        "Cmp. Hotel": 300
+    },
+    "Les Rambles": {
+        "Ll. Casa": 35,
+        "Ll. Hotel": 30,
+        "Cmp. Trrny": 70,
+        "Cmp. Casa": 450,
+        "Cmp. Hotel": 310
+    },
+    "Pl. Catalunya": {
+        "Ll. Casa": 35,
+        "Ll. Hotel": 30,
+        "Cmp. Trrny": 70,
+        "Cmp. Casa": 475,
+        "Cmp. Hotel": 320
+    },
+    "P. Àngel": {
+        "Ll. Casa": 40,
+        "Ll. Hotel": 35,
+        "Cmp. Trrny": 80,
+        "Cmp. Casa": 500,
+        "Cmp. Hotel": 330
+    },
+    "Via Augusta": {
+        "Ll. Casa": 40,
+        "Ll. Hotel": 35,
+        "Cmp. Trrny": 80,
+        "Cmp. Casa": 525,
+        "Cmp. Hotel": 250
+    },
+    "Balmes": {
+        "Ll. Casa": 50,
+        "Ll. Hotel": 40,
+        "Cmp. Trrny": 80,
+        "Cmp. Casa": 550,
+        "Cmp. Hotel": 350
+    },
+    "Pg. de Gràcia": {
+        "Ll. Casa": 50,
+        "Ll. Hotel": 50,
+        "Cmp. Trrny": 80,
+        "Cmp. Casa": 525,
+        "Cmp. Hotel": 360
+    }
+}
