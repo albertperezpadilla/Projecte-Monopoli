3da5685dc000d3df01759a800ded4ab45615f370
Albert Pérez Padilla <152429134+albertperezpadilla@users.noreply.github.com>
2024-10-14 16:04:42 +0200

    .

--- Monopoly.py
+++ Monopoly.py
@@ -1,1 +1,13 @@
 #Aqui haremos el monopoly
+
+import random
+
+#Variables del comienzo de la partida
+banc = 0
+if banc < 500000:
+    banc += 1000000
+
+dgroc = 2000
+dtaronja = 2000
+dvermell = 2000
+dblau= 2000
